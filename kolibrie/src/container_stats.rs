7843e78405bb244c130a4105d0729f55f5c75ca1
Flor De Meulemeester <flor.de.meulemeester@gmail.com>
2026-03-20 12:42:42 +0100

    Compare containers now

--- kolibrie/src/container_stats.rs
+++ kolibrie/src/container_stats.rs
@@ -3,16 +3,16 @@
  * These stats include total amount of triples and some cardinalities.
  * This is inspired by the DatabaseStats struct used in the streamertail_optimizer.
  */
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use shared::triple::Triple;
 use crate::rsp::s2r::*;
 
 #[derive(Debug)]
 pub struct ContainerStats {
-    pub total_triples: u64,
-    pub predicate_cardinalities: HashMap<u32, u64>, // meaning u32 appears a number of u64 times in the container
-    pub subject_cardinalities: HashMap<u32, u64>,
-    pub object_cardinalities: HashMap<u32, u64>,
+    pub total_triples: i64,
+    pub predicate_cardinalities: HashMap<u32, i64>, // meaning u32 appears a number of i64 times in the container
+    pub subject_cardinalities: HashMap<u32, i64>,
+    pub object_cardinalities: HashMap<u32, i64>,
 }
 
 impl ContainerStats {
@@ -28,11 +28,11 @@ impl ContainerStats {
 
     // Gather stats for a given container
     pub fn gather_stats(container: &ContentContainer<Triple>) -> Self {
-        let total_triples: u64 = container.len().try_into().unwrap();
+        let total_triples: i64 = container.len().try_into().unwrap();
         // Build cardinality maps
-        let mut predicate_cardinalities: HashMap<u32, u64> = HashMap::new();
-        let mut subject_cardinalities: HashMap<u32, u64> = HashMap::new();
-        let mut object_cardinalities: HashMap<u32, u64> = HashMap::new();
+        let mut predicate_cardinalities: HashMap<u32, i64> = HashMap::new();
+        let mut subject_cardinalities: HashMap<u32, i64> = HashMap::new();
+        let mut object_cardinalities: HashMap<u32, i64> = HashMap::new();
 
         // iterate over the triples
         for triple in container.iter() {
@@ -52,7 +52,7 @@ impl ContainerStats {
     }
 
     // Get the total amount of triples
-    pub fn get_total_triples(&self) -> u64 {
+    pub fn get_total_triples(&self) -> i64 {
         self.total_triples
     }
 
@@ -72,17 +72,17 @@ impl ContainerStats {
     }
 
     // Gets the cardinality for an object
-    pub fn get_object_cardinality(&self, object: u32) -> u64 {
+    pub fn get_object_cardinality(&self, object: u32) -> i64 {
         self.object_cardinalities.get(&object).copied().unwrap_or(0)
     }
 
     // Gets the cardinality for a predicate
-    pub fn get_predicate_cardinality(&self, predicate: u32) -> u64 {
+    pub fn get_predicate_cardinality(&self, predicate: u32) -> i64 {
         self.predicate_cardinalities.get(&predicate).copied().unwrap_or(0)
     }
 
     // Gets the cardinality for an subject
-    pub fn get_subject_cardinality(&self, subject: u32) -> u64 {
+    pub fn get_subject_cardinality(&self, subject: u32) -> i64 {
         self.subject_cardinalities.get(&subject).copied().unwrap_or(0)
     }
 
@@ -92,6 +92,40 @@ impl ContainerStats {
         *self.predicate_cardinalities.entry(predicate).or_insert(0) += 1;
         *self.subject_cardinalities.entry(subject).or_insert(0) += 1;
         *self.object_cardinalities.entry(object).or_insert(0) += 1;
+    }
+
+    /*
+    Calculate the difference of two hashmaps
+    For example a = {1: 3, 2: 10} and b = {1: 2, 3: 5}
+    Then result = {1: 1, 2: 10, 3: -5}
+    */
+    fn diff_of_hashmaps(a: &HashMap<u32, i64>, b: &HashMap<u32, i64>) -> HashMap<u32, i64> {
+    let all_keys: HashSet<_> = a.keys().chain(b.keys()).collect(); // Get all unique keys from both maps
+    all_keys
+        .into_iter()
+        .map(|&key| {
+            let val_a = a.get(&key).copied().unwrap_or(0);
+            let val_b = b.get(&key).copied().unwrap_or(0);
+            (key, val_a - val_b)
+        })
+        .collect()
+    }
+
+    // result is basically self - previousStats for all elements
+    pub fn compare(&mut self, previous_stats: ContainerStats) -> Self {
+        let total_triples = self.get_total_triples() - previous_stats.get_total_triples();
+
+        let predicate_cardinalities = Self::diff_of_hashmaps(&self.predicate_cardinalities, &previous_stats.predicate_cardinalities);
+        let subject_cardinalities = Self::diff_of_hashmaps(&self.subject_cardinalities, &previous_stats.subject_cardinalities);
+        let object_cardinalities = Self::diff_of_hashmaps(&self.object_cardinalities, &previous_stats.object_cardinalities);
+
+        // return stats struct
+        Self {
+            total_triples, // difference in the amount of triples from current windows versus previous window
+            predicate_cardinalities,
+            subject_cardinalities,
+            object_cardinalities,
+        }
     }
 
 }
@@ -154,5 +188,73 @@ mod tests {
         assert_eq!(stats.get_predicate_cardinality(2), 2);
         assert_eq!(stats.get_subject_cardinality(10), 1);
         assert_eq!(stats.get_subject_cardinality(11), 2);
+    }
+
+    #[test]
+    fn test_compare_function() {
+        // previous window
+        let triples = vec![
+            Triple {
+                subject: 1,
+                predicate: 11,
+                object: 103 
+            },
+            Triple {
+                subject: 1,
+                predicate: 10,
+                object: 101,
+            },
+            Triple {
+                subject: 2,
+                predicate: 11,
+                object: 100,
+            }
+        ];
+
+        let container = ContentContainer::from_items(triples, 1);
+        let previous_stats = ContainerStats::gather_stats(&container);
+        
+        // current window 
+        let triples = vec![
+            Triple {
+                subject: 1,
+                predicate: 10,
+                object: 100 
+            },
+            Triple {
+                subject: 2,
+                predicate: 10,
+                object: 100,
+            },
+            Triple {
+                subject: 3,
+                predicate: 10,
+                object: 101,
+            },
+            Triple {
+                subject: 3,
+                predicate: 11,
+                object: 102,
+            }
+        ];
+
+        let container = ContentContainer::from_items(triples, 1);
+        let mut current_stats = ContainerStats::gather_stats(&container);
+
+        let diff_stats = current_stats.compare(previous_stats);
+
+        assert_eq!(diff_stats.get_total_triples(), 1); // 4 - 3
+        
+        assert_eq!(diff_stats.get_subject_cardinality(1), -1);
+        assert_eq!(diff_stats.get_subject_cardinality(2), 0);
+        assert_eq!(diff_stats.get_subject_cardinality(3), 2);
+
+        assert_eq!(diff_stats.get_predicate_cardinality(10), 2);
+        assert_eq!(diff_stats.get_predicate_cardinality(11), -1);
+
+        assert_eq!(diff_stats.get_object_cardinality(100), 1);
+        assert_eq!(diff_stats.get_object_cardinality(101), 0);
+        assert_eq!(diff_stats.get_object_cardinality(102), 1);
+        assert_eq!(diff_stats.get_object_cardinality(103), -1);
     }
 }
